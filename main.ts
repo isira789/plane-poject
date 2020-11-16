ffc77a8e2d8b55d49b145431d514c066c2c2ce1a
isira789 <74458393+isira789@users.noreply.github.com>
2020-11-16 10:10:12 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,66 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        # # # # #
+        # . # . #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        # . # . #
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        # . # . #
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        # # # # #
+        # . # . #
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        # . # . #
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # . .
+        . # # # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . # # # .
+        # # # # #
+        `)
+    basic.pause(100)
+})
